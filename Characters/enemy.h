af02011e6d3de6c6cd9e93bd0f256bb2a863a98b
Kareem Mohammed <109914324+karemsaeed21@users.noreply.github.com>
2025-05-10 16:16:52 +0300

    Update enemy.h

--- Characters/enemy.h
+++ Characters/enemy.h
@@ -1,4 +1,27 @@
-// character.h
+// // character.h
+// #ifndef ENEMY_H
+// #define ENEMY_H
+
+// void Head();
+// void Face();
+// void Body();
+
+// void RightLeg();
+// void LeftLeg();
+// void drawCreeper();
+// // struct Enemy
+// // {
+// //     float x, y;
+// //     bool isVisible;
+// //     Enemy(float startX, float startY) : x(startX), y(startY), isVisible(true) {}
+// // };
+// struct Enemy
+// {
+//     float x, y;
+//     float speed;
+// };
+// #endif
+
 #ifndef ENEMY_H
 #define ENEMY_H
 
@@ -9,15 +32,20 @@ void Body();
 void RightLeg();
 void LeftLeg();
 void drawCreeper();
-// struct Enemy
-// {
-//     float x, y;
-//     bool isVisible;
-//     Enemy(float startX, float startY) : x(startX), y(startY), isVisible(true) {}
-// };
-struct Enemy
+
+class Enemy
 {
-    float x, y;
+public:
+    float x;
+    float y;
     float speed;
+    float rotation;
+    float rotationSpeed; // Add rotation speed
+    bool isActive;
+    bool isFalling;
+    bool hasPassed;
+
+    Enemy(float x, float y, float speed);
 };
+
 #endif
